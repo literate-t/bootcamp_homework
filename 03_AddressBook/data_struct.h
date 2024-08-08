872e70b0bcfe881daece020410cb2a54a4713e3f
literate <gaekoon@gmail.com>
2024-08-08 15:58:03 +0900

    feat: 데이터와 자료구조를 변경한다

--- 03_AddressBook/data_struct.h
+++ 03_AddressBook/data_struct.h
@@ -1,22 +1,23 @@
 #pragma once
 #include <stdbool.h>
 
-typedef struct USERDATA {
-	int age;
+typedef struct USERDATA {	
 	char name[32];
+	int age;
+	char address[32];
 	char phone[32];
 } USERDATA;
 
 typedef struct NODE
 {
 	bool is_new;
-	bool is_modified;
+	bool is_updated;
 	bool is_deleted;
 
 	char* key;
 	void* data_cache;       // USERDATA*
 	unsigned int data_size; // sizeof USERDATA
-	unsigned int offset;    // 파일 상의 위치
+	int offset;				// 파일 상의 위치
 
 	struct NODE* next_ptr;
 	struct NODE* prev_ptr;
